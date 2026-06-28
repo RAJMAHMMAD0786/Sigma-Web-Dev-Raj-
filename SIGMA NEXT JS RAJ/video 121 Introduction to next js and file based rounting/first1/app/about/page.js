87c2d430faf730f35cda6e2cf56b427bf119aed1
RAJMAHMMAD0786 <rajalam05588@gmail.com>
2026-06-28 13:07:02 +0530

    Today I Study Introduction to next js

---
+++ SIGMA NEXT JS RAJ/video 121 Introduction to next js and file based rounting/first1/app/about/page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      
+      <h1 className="text-xl text-blue-500">Problem Solve by Next.js</h1>
+
+      <ul>
+        <li>Full Stack Solutions</li>
+        <li>File Base Routing</li>
+        <li>Additiona Features Like router from next/navigation</li>
+        <li>Optimize Rendering</li>
+      </ul>
+
+    </div>
+  )
+}
+
+export default page
---
+++ SIGMA NEXT JS RAJ/video 121 Introduction to next js and file based rounting/first1/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      I am Navbar of this page
+    </div>
+  )
+}
+
+export default Navbar
